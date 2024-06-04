60159283c9b33672f3172263295c37cd4d886c1f
Rohit Singh <rohit.singh.33521@gmail.com>
2024-06-04 21:58:07 +0530

    added firebase + login data to firestore

---
+++ firebaseConfig.js
@@ -0,0 +1,22 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore, doc, setDoc , getDocs ,collection} from 'firebase/firestore';
+import { GoogleAuthProvider, signInWithPopup,getAuth , signOut} from 'firebase/auth';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+const auth = getAuth(firebaseApp);
+
+export default { firebaseConfig,firebaseApp, db, doc , setDoc ,getFirestore, getDocs, collection,auth, GoogleAuthProvider, signInWithPopup,};
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require("cors");
+import firebase from './firebaseConfig.js';
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from "cors";
+
+const { db, doc, setDoc, getDocs, collection, firebaseConfig } = firebase;
 
 const app = express();
 const port = process.env.PORT || 5000; 
@@ -8,10 +11,24 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-app.post('/api/login', (req, res) => {
-  console.log(req.body);
-  const responseData = { message: 'User data received successfully' };
-  res.status(200).json(responseData);
+var userId;
+app.post('/api/login', async (req, res) => {
+    const responseData = { message: 'User data received successfully' };
+    res.status(200).json(responseData);
+
+    userId = req.body.uid;
+
+    const data = {
+        userId: userId ,
+        email : req.body.email,
+        photoURL : req.body.photoURL,
+        displayName : req.body.displayName
+    };
+
+    console.log(data);
+
+    setDoc(doc(db, 'users', userId), data, { merge: true });
+
 });
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
