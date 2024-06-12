22bf47bf10a218a0b39b798304965eef3f01dd85
Rohit Singh <rohit.singh.33521@gmail.com>
2024-06-12 21:10:50 +0530

    upd

--- server.js
+++ server.js
@@ -5,15 +5,20 @@ import bodyParser from 'body-parser';
 import cors from "cors";
 import Razorpay from 'razorpay';
 
-const { db, doc, setDoc,getDocs } = firebase;
+const {
+    db,
+    doc,
+    setDoc,
+    getDocs
+} = firebase;
 
 const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_KEY_SECRET
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_KEY_SECRET
 });
 
 const app = express();
-const port = process.env.PORT || 5000; 
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -21,20 +26,24 @@ app.use(bodyParser.json());
 var userId;
 
 app.post('/api/login', async (req, res) => {
-    const responseData = { message: 'User data received successfully' };
+    const responseData = {
+        message: 'User data received successfully'
+    };
     res.status(200).json(responseData);
 
     userId = req.body.uid;
 
     const data = {
-        userId: userId ,
-        email : req.body.email,
-        photoURL : req.body.photoURL,
-        displayName : req.body.displayName
+        userId: userId,
+        email: req.body.email,
+        photoURL: req.body.photoURL,
+        displayName: req.body.displayName
     };
 
     console.log(data);
-    setDoc(doc(db, 'users', userId), data, { merge: true });
+    setDoc(doc(db, 'users', userId), data, {
+        merge: true
+    });
 
 });
 
@@ -45,64 +54,90 @@ app.get('/api/getSubmissionCalendar/:username', async (req, res) => {
     try {
         const response = await axios.get(url);
         const leetcodeData = response.data;
-        
-        res.status(200).json({ leetcodeData: leetcodeData });
+
+        res.status(200).json({
+            leetcodeData: leetcodeData
+        });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: 'Error retrieving submission calendar' });
+        res.status(500).json({
+            message: 'Error retrieving submission calendar'
+        });
     }
 });
 
 
 app.post('/api/submitProfileDetails', async (req, res) => {
-    const { userId, personalInfo, socialLinks } = req.body;
-  
-    const data = {
-      userId,
-      personalInfo,
-      socialLinks
-    };
-  
-    try {
-      console.log(data);
-      res.status(200).json({ message: 'Profile details submitted successfully' });
-    } catch (error) {
-      console.error('Error saving data:', error);
-      res.status(500).json({ message: 'Error saving profile details' });
-    }
-  });
+    const {
+        userId,
+        personalInfo,
+        socialLinks
+    } = req.body;
 
-  app.post('/createPayment', async (req, res) => {
-    const { amount, currency, receipt } = req.body;
+    const data = {
+        userId,
+        personalInfo,
+        socialLinks
+    };
+
+    try {
+        console.log(data);
+        res.status(200).json({
+            message: 'Profile details submitted successfully'
+        });
+    } catch (error) {
+        console.error('Error saving data:', error);
+        res.status(500).json({
+            message: 'Error saving profile details'
+        });
+    }
+});
+
+app.post('/createPayment', async (req, res) => {
+    const {
+        amount,
+        currency,
+        receipt
+    } = req.body;
     try {
         const order = await razorpay.orders.create({
             amount: amount * 100,
             currency,
             receipt
         });
-  
-        res.json({ orderId: order.id, amount: order.amount });
+
+        res.json({
+            orderId: order.id,
+            amount: order.amount
+        });
     } catch (error) {
         console.error('Error creating order:', error);
         res.status(500).send('Error creating order');
     }
-  });
-  
-  app.post('/paymentSuccess', async (req, res) => {
-    const { orderId, paymentId, signature } = req.body;
-  
+});
+
+app.post('/paymentSuccess', async (req, res) => {
+    const {
+        orderId,
+        paymentId,
+        signature
+    } = req.body;
+
     const generatedSignature = crypto.createHmac('sha256', 'RAZORPAY_KEY_SECRET').update(orderId + '|' + paymentId).digest('hex');
     if (generatedSignature === signature) {
-  
+
         res.send('Payment successful');
     } else {
         res.send('Payment failed');
     }
-  });
+});
 
-  app.post('/api/buildrUsername', (req, res) => {
+app.post('/api/buildrUsername', (req, res) => {
     try {
-        const { userId, username } = req.body;
+        const {
+            userId,
+            username
+        } = req.body;
 
         const data = {
             userId: userId,
@@ -111,16 +146,21 @@ app.post('/api/submitProfileDetails', async (req, res) => {
 
         console.log(data);
 
-        setDoc(doc(db, 'users', userId), data, { merge: true });
-        
-        res.status(200).send({ message: 'Username updated successfully' });
+        setDoc(doc(db, 'users', userId), data, {
+            merge: true
+        });
+
+        res.status(200).send({
+            message: 'Username updated successfully'
+        });
     } catch (error) {
         console.error('Error:', error);
-        res.status(500).send({ error: 'An internal server error occurred' });
+        res.status(500).send({
+            error: 'An internal server error occurred'
+        });
     }
 });
 
-  
+
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
-
